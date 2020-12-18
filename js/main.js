1918bd312bb8fadc9171bff6d1e6dc1a84b639f1
helenohyeah <ouyang.helen@gmail.com>
2020-12-18 12:30:38 -0500

    Create html content for project section

--- js/main.js
+++ js/main.js
@@ -7,12 +7,12 @@ const { navs, about, projects } = Data;
 // console.log(about);
 
 const generateNav = (data) => {
-  const html = data.map(item => ``)
-  return data.map(item => `<a href="${item.link}">${item.name}</a>`).join('');
+  const html = data.map(item => `<a href="${item.link}">${item.name}</a>`).join('');
+  return html;
 };
 
 const generateAbout = (data) => {
-  return `
+  const html = `
   <div id="profile">
     <img src="${data.imgSrc}" alt="Profile picture">
     <h1>${data.title}<h1>
@@ -23,7 +23,35 @@ const generateAbout = (data) => {
     <p>${data.bioBody}</p>
   </div>
   `;
+  return html;
 };
+
+const generateProjects = (data) => {
+  const projects = data.map(project => {
+    let projectHtml = `
+    <article class="project">
+      <h4>${project.title}</h4>
+      <img src="${project.imgSrc}" alt="${project.title}. ${project.description}" />
+      <p>${project.description}</p>
+    `;
+    if (project.demoUrl) {
+      projectHtml += `
+      <button onclick="window.location.href='${project.demoUrl}'">Demo</button>
+      `;
+    }
+    projectHtml += `
+    <button onclick="window.location.href='${project.githubUrl}'">GitHub</button>
+    </article>
+    `;
+    return projectHtml;
+  }).join('');
+  const html = `
+  <h2>My Recent Work</h2>
+  <p>Check out a few projects that I've been working on<p>
+  ${projects}
+  `;
+  return html;
+}
 
 // Render nav
 const $nav = document.querySelector('nav');
@@ -40,3 +68,8 @@ const aboutSection = generateAbout(about);
 // console.log(aboutSection);
 // $about.innerHTML = aboutSection;
 $about.insertAdjacentHTML('afterbegin', aboutSection);
+
+// Render projects
+const $projects = document.querySelector('.projects');
+const projectSection = generateProjects(projects);
+$projects.insertAdjacentHTML('afterbegin', projectSection);
